ef31bacb8f7f7d180277bba2abe0989c22096e90
faissal17 <faissalaoukacha@gmail.com>
2023-09-20 15:02:34 +0100

    store names in local storage

--- js/script.js
+++ js/script.js
@@ -3,7 +3,11 @@ function validatePlayers() {
   var player2 = document.querySelector(".player2");
   var player1Error = document.querySelector("#player1Error");
   var player2Error = document.querySelector("#player2Error");
-  if (player1.value.trim() === "") {
+
+  var player1Name = player1.value.trim();
+  var player2Name = player2.value.trim();
+
+  if (player1Name === "") {
     player1.classList.add("error");
     player1Error.textContent = "Player 1 name cannot be empty";
     return;
@@ -11,7 +15,7 @@ function validatePlayers() {
     player1.classList.remove("error");
     player1Error.textContent = "";
   }
-  if (player2.value.trim() === "") {
+  if (player2Name === "") {
     player2.classList.add("error");
     player2Error.textContent = "Player 2 name cannot be empty";
     return;
@@ -19,5 +23,11 @@ function validatePlayers() {
     player2.classList.remove("error");
     player2Error.textContent = "";
   }
-  window.location.href = "game.html";
+  window.localStorage.setItem("player1Name", player1Name);
+  window.localStorage.setItem("player2Name", player2Name);
+
+  var storedPlayer1Name = window.localStorage.getItem("player1Name");
+  var storedPlayer2Name = window.localStorage.getItem("player2Name");
+
+  // window.location.href = "game.html";
 }
