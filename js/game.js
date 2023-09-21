8cf241acd4f1ad70c7a86d71674637e0ea7d62ba
faissal17 <faissalaoukacha@gmail.com>
2023-09-21 09:48:45 +0100

    create the game board 20*20

--- js/game.js
+++ js/game.js
@@ -4,5 +4,29 @@ var storedPlayer2Name = window.localStorage.getItem("player2Name");
 document.getElementById("player1Name").innerHTML = storedPlayer1Name;
 document.getElementById("player2Name").innerHTML = storedPlayer2Name;
 
-const game = document.querySelector(".gameBoard");
-const info = document.getElementById("info");
+const game = document.querySelector("#gameBoard");
+const info = document.querySelector("#info");
+
+const StartCells = () => {
+  const CellsArray = [];
+
+  for (let i = 0; i < 400; i++) {
+    CellsArray.push("");
+  }
+  return CellsArray;
+};
+
+function CreateBoard() {
+  const gameBoard = document.getElementById("gameBoard");
+
+  const cells = StartCells();
+
+  cells.forEach((cell, index) => {
+    const cellElement = document.createElement("div");
+    cellElement.classList.add("square");
+    cellElement.setAttribute("data-index", index);
+    gameBoard.appendChild(cellElement);
+  });
+}
+
+CreateBoard();
