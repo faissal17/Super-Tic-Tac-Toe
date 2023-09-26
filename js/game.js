f5e474b481fae3fa0e2e4e0c36cf64acb9eca149
faissal17 <faissalaoukacha@gmail.com>
2023-09-26 14:19:07 +0100

    finish the diagonale logic

--- js/game.js
+++ js/game.js
@@ -98,4 +98,28 @@ function Winner(selectedCell) {
   if (streak >= 5) {
     alert("Winner!");
   }
+  for (let i = selectedCellId - 84; i <= selectedCellId + 84; i += 21) {
+    if (i <= boeardSize && i >= 0) {
+      if (cellSquare[i] && cellSquare[i].hasChildNodes()) {
+        if (
+          selectedCell.children[0].getAttribute("class") ===
+          cellSquare[i].children[0].getAttribute("class")
+        ) {
+          console.log("it has a child");
+          streak++;
+          if (streak >= 5) break;
+        } else {
+          streak = 0;
+          console.log("there is a child but they are not the same");
+        }
+      } else {
+        streak = 0;
+        console.log("the square is empty");
+      }
+    }
+  }
+  console.log(streak);
+  if (streak >= 5) {
+    alert("Winner!");
+  }
 }
